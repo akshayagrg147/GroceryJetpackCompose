e7191ca87b9721ce8b7703169c4ef6766c931e0b
akshay <akshay.g@antino.io>
2023-07-25 16:01:56 +0530

    add token expire part

---
+++ app/src/main/java/com/grocery/groceryapp/Utils/animation.kt
@@ -0,0 +1,25 @@
+package com.grocery.groceryapp.Utils
+
+import androidx.compose.animation.AnimatedContentScope
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+
+// Example function to define custom enter and exit animations
+@OptIn(ExperimentalAnimationApi::class)
+fun AnimatedContentScope<String>.customSlideAnimations() {
+    // Define the enter animation using slideInVertically
+    // Slide in from the top
+    slideInVertically(
+        initialOffsetY = { -it },
+        animationSpec = tween(durationMillis = 500)
+    )
+
+    // Define the exit animation using slideOutVertically
+    // Slide out to the bottom
+    slideOutVertically(
+        targetOffsetY = { it },
+        animationSpec = tween(durationMillis = 500)
+    )
+}
